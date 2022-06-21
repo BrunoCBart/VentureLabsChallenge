e4fbfe4efbeb4867b1c0b12e0a236342971274f7
Bruno Bartolomeu <bruno.cbartlomeu@gmail.com>
2022-06-21 11:30:52 -0300

    feat: add click events for progress steps

--- src/components/VentureSteps/VentureSteps.tsx
+++ src/components/VentureSteps/VentureSteps.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 import './ventureSteps.css'
 import Contact from '../svgs/Contact'
 import Address from '../svgs/Address'
@@ -11,7 +11,7 @@ interface Step {
   active: boolean
 }
 
-const steps: Step[] = [
+const InitialSteps: Step[] = [
   {
     label: 'Contact',
     icon: <Contact className="progress-icon"/>,
@@ -33,19 +33,66 @@ const steps: Step[] = [
     icon: <Done className="progress-icon"/>,
     active: false
   }
+
 ]
 
 function VentureSteps () {
+  const [steps, setSteps] = useState(InitialSteps)
+  const [currentStep, setCurrentStep] = useState(0)
+
+  const onNextAndPrevStep = () => {
+    const newSteps = steps.map((step, i) => {
+      if (i <= currentStep) {
+        return {
+          ...step,
+          active: true
+        }
+      }
+      return {
+        ...step,
+        active: false
+      }
+    })
+    setSteps(newSteps)
+  }
+
+  const onStepCircleButtonClick = (i: number) => {
+    if (currentStep + 1 === i || currentStep - 1 === i) { setCurrentStep(i) }
+  }
+
+  useEffect(() => {
+    const progress: any = document.querySelector('#progress')
+    progress.style.width = ((currentStep) / (steps.length - 1)) * 100 + '%'
+    onNextAndPrevStep()
+  }, [currentStep])
+
   return (
     <div className="ventureSteps">
-      <div id="progress"></div>
-      {steps.map((step: Step, index:number) => {
-        return (
-          <div className={`ventureStep circle ${step.active ? 'active' : ''}`} key={index}>
-            {step.icon}
-          </div>
-        )
-      })}
+        <div className="ventureSteps__buttons">
+          <button className="btn"
+          onClick={() => setCurrentStep(currentStep > 0 ? currentStep - 1 : 0)}
+          >
+            Voltar
+          </button>
+          <button className="btn"
+          onClick={() => setCurrentStep(steps.length - 1 === currentStep ? currentStep : currentStep + 1)}
+          >
+            Próximo
+          </button>
+        </div>
+      <div className="ventureSteps__container">
+        <div id="progress"></div>
+        {steps.map((step: Step, index:number) => {
+          return (
+            <div className={`ventureStep circle ${step.active ? 'active' : ''}`}
+              key={index}
+              onClick={() => onStepCircleButtonClick(index)}
+            >
+              {step.icon}
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
